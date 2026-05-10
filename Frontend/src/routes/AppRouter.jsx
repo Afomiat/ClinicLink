90d342fb1687bb0d43deec8633abc3cc6d5231a9
Afomiat <afomiat79@gmail.com>
2026-05-11 00:18:24 +0300

    feat: implement AppRouter with nested routing for doctor and patient dashboards

--- Frontend/src/routes/AppRouter.jsx
+++ Frontend/src/routes/AppRouter.jsx
@@ -1,5 +1,4 @@
-// src/routes/AppRouter.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { DarkModeProvider } from '../Pages/Dashboards/Docters/DarkModeContext';
 import Home from '../Pages/Home/Home';
 import DoctorAppRouter from './DoctorAppRouter';
@@ -9,8 +8,9 @@ const AppRouter = () => {
   return (
     <DarkModeProvider>
       <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
+        {/* Public routes - Redirecting to Patient Dashboard as requested */}
+        <Route path="/" element={<Navigate to="/patient" replace />} />
+        <Route path="/home" element={<Home />} />
         
         {/* Doctor dashboard routes */}
         <Route path="/doctor/*" element={<DoctorAppRouter />} />
